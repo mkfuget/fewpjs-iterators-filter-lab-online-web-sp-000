b61e52ad38de6f3de35fbbd01d697755c631454b
Michael Fuget <michaelfuget@gmail.com>
2021-02-14 08:22:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function findMatching(drivers, stringInput)
     return n.toLowerCase() === stringInput.toLowerCase()
   })
 }
+function function fuzzyMatch(drivers, stringInput)
+{
+  return drivers.filter(n => {
+    return n.substring(0, stringInput.length - 1) === stringInput.toLowerCase()
+  })
+}
